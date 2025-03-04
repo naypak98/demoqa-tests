490c03cb00ef5ad1606303bc04825511f2114838
Anastasiia <a.naypak@gmail.com>
2025-03-04 19:45:27 +0400

    added TextBoxTests

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -16,6 +16,7 @@ public class TextBoxTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.holdBrowserOpen = true;
+
     }
 
     @Test
