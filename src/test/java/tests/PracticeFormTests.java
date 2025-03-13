947450f3a0cd134af76d76c9418553b994444b01
Anastasiia <a.naypak@gmail.com>
2025-03-14 01:01:02 +0400

    added some tests to PracticeFormTests

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
@@ -20,6 +22,7 @@ public class PracticeFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = true;
+
     }
 
     @Test
@@ -29,20 +32,37 @@ public class PracticeFormTests {
         $("#lastName").setValue("Popova");
         $("#userEmail").setValue("anna@popova.com");
         $("label[for='gender-radio-2']").click();
-        $("#userNumber").setValue("+12345");
+        $("#userNumber").setValue("9123454562");
         //date
         $("#dateOfBirthInput").click(); // Открываем календарь
         $(".react-datepicker__month-select").selectOption("January"); // Выбираем месяц
         $(".react-datepicker__year-select").selectOption("1995"); // Выбираем год
         $(".react-datepicker__day--021").click(); // Кликаем по числу 21
         //
-
+        //Subjects
         $("#subjectsInput").click();
         $("#subjectsInput").setValue("M");
-        $(".subjects-auto-complete__menu-list").$(byText("Maths")).click();
+        $(".subjects-auto-complete__menu-list").$(byText("Maths")).click(); //setTimeout(function() {debugger;}, 5000)
+        //
+        $("label[for='hobbies-checkbox-2']").click();
+        File file = new File("src/testData/kotik.jpg");
+        $("#uploadPicture").uploadFile(file);
+        $("#currentAddress").setValue("Test city, street 1");
+        //state
+        $("#state").click();
+        $(".css-26l3qy-menu").find(byText("Uttar Pradesh")).click();
+        //city
+        $("#city").click();
+        $(".css-26l3qy-menu").find(byText("Agra")).click();
 
-//        subjects-auto-complete__menu-list subjects-auto-complete__menu-list--is-multi css-11unzgr
-        //setTimeout(function() {debugger;}, 5000)
+        $("#submit").click();
+
+
+
+
+
+
+
 
 
 
