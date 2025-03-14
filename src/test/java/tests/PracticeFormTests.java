155fa6e19ccb3c9b64176c044640f40669ebaf1a
Anastasiia <a.naypak@gmail.com>
2025-03-14 15:36:19 +0400

    edited fillPracticeForm test

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -3,16 +3,13 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
+
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
 
@@ -21,7 +18,7 @@ public class PracticeFormTests {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
+//        Configuration.holdBrowserOpen = true;
 
     }
 
@@ -44,7 +41,7 @@ public class PracticeFormTests {
         $("#subjectsInput").setValue("M");
         $(".subjects-auto-complete__menu-list").$(byText("Maths")).click(); //setTimeout(function() {debugger;}, 5000)
         //
-        $("label[for='hobbies-checkbox-2']").click();
+        $("label[for='hobbies-checkbox-2']").click(); //reading
         File file = new File("src/testData/kotik.jpg");
         $("#uploadPicture").uploadFile(file);
         $("#currentAddress").setValue("Test city, street 1");
@@ -57,20 +54,18 @@ public class PracticeFormTests {
 
         $("#submit").click();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //check result
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $$("tbody tr").findBy(text("Student Name")).$$("td").get(1).shouldHave(text("Anna Popova"));
+        $$("tbody tr").findBy(text("Student Email")).$$("td").get(1).shouldHave(text("anna@popova.com"));
+        $$("tbody tr").findBy(text("Gender")).$$("td").get(1).shouldHave(text("Female"));
+        $$("tbody tr").findBy(text("Mobile")).$$("td").get(1).shouldHave(text("9123454562"));
+        $$("tbody tr").findBy(text("Date of Birth")).$$("td").get(1).shouldHave(text("21 January,1995"));
+        $$("tbody tr").findBy(text("Subjects")).$$("td").get(1).shouldHave(text("Maths"));
+        $$("tbody tr").findBy(text("Hobbies")).$$("td").get(1).shouldHave(text("Reading"));
+        $$("tbody tr").findBy(text("Picture")).$$("td").get(1).shouldHave(text("kotik.jpg"));
+        $$("tbody tr").findBy(text("Address")).$$("td").get(1).shouldHave(text("Test city, street 1"));
+        $$("tbody tr").findBy(text("State and City")).$$("td").get(1).shouldHave(text("Uttar Pradesh Agra"));
 
 
 
